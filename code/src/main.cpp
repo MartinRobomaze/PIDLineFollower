21a3ee4be707acbad98eb5fad34ce42c374fad4b
Martinlinux1 <martin.robomaze@yandex.com>
2019-03-01 15:03:00 +0100

    Commented main.cpp

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -11,32 +11,43 @@
 #include "Motors.h"
 #include "LightSensors.h"
 
-
+// Motor and light sensor pins.
 int motorsPins[4] = {3, 5, 6, 9};
 int lightSensorsPins[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
+int numberLightSensors = 8;
+
+// Kp, Kd constants - you hzve to experiment with them to have good results.
 int Kp = 1;
 int Kd = 1;
+// Last error.
 int lastError = 0;
 
+// Calibration values for light sensors - zou have to set them depending on the light conditions
 int blackSensorValue = 0;
 int whiteSensorValue = 0;
 
+// Base speed for motors.
 int baseSpeed = 150;
 
+// Function declaration.
 void calculatePID(int error, int Kp, int Kd, int *speedA, int *speedB);
 void readLightSensors(int *sensorsValueArr);
 int getError(int *sensorsReadValue);
 
 void setup() {
+  // Start serial communication.
   Serial.begin(9600);
 
+  // Setup motors.
   setupMotors(motorPins);
+  // Setup light sensors.
   setupSensors(lightSensorsPins);
 }
 
 void loop() {
-  int lightSensorsReading[8];
+  // Defin
+  int lightSensorsReading[numberLightSensors];
 
   readLightSensors(lightSensorsReading);
 
@@ -60,8 +71,8 @@ void calculatePID(int error, int Kp, int Kd, int *speedA, int *speedB) {
 }
 
 void readLightSensors(int *sensorsValueArr) {
-  int sensorValues[8];
-  for (int i = 0; i < 8; i++) {
+  int sensorValues[numberLightSensors];
+  for (int i = 0; i < numberLightSensors; i++) {
     if (readLightSensor(i) <= blackSensorValue) {
       sensorValues[i] = 1;
     }
@@ -77,7 +88,7 @@ void readLightSensors(int *sensorsValueArr) {
 int getError(int *sensorsReadValue) {
   int error = -4;
 
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < numberLightSensors; i++) {
     if (sensorsReadValue[i] == 1) {
       error++;
     }
