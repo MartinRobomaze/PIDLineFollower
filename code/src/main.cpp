ff4f91ecf5501eafd4f2bab1bc9e7c4a8b944db4
Martinlinux1 <martin.robomaze@yandex.com>
2019-03-07 18:23:45 +0100

    Changed bluetooth communicating protocol.

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,8 +4,6 @@
   Creators: Martinlinux1, Kiriwer
   Version: 0.1a
 */
-#include "Arduino.h"
-#include "SoftwareSerial.h"
 #include "MazeCar.h"
 #include "Motors.h"
 #include "LightSensors.h"
@@ -18,6 +16,8 @@ int numberLightSensors = 8;
 
 int BTRx = 12;
 int BTTx = 11;
+
+int buttonPin = 2;
 
 SoftwareSerial BT(BTRx, BTTx);
 
@@ -33,18 +33,30 @@ int whiteSensorValue = 100;
 
 // Base speed for motors.
 int baseSpeed = 150;
+bool running = false;
+
+int previous = 1;
+long time = 0;
+int debounce = 200;
 
 // Function declaration.
 void calculatePID(int error, int Kp, int Kd, int *speedA, int *speedB);
 int readLightSensorDigital(int sensor);
 int getError(int *sensorsReadValue);
 void getBluetoothData(float *Kp, float *Kd);
-
+void interrupt();
+void save();
 
 void setup() {
+  Kp = EEPROM.read(0);
+  Kd = EEPROM.read(1);
+  // Set interrupt button pin as INPUT_PULLUP.
+  pinMode(buttonPin, INPUT_PULLUP);
+
   // Start serial communication.
   Serial.begin(9600);
 
+  // Start bluetooth communication.
   BT.begin(9600);
 
   // Setup motors.
@@ -54,35 +66,56 @@ void setup() {
 }
 
 void loop() {
-  // Light sensors readings array.
-  int lightSensorsReading[numberLightSensors];
+  // If running = true.
+  if (running) {
+    // Light sensors readings array.
+    int lightSensorsReading[numberLightSensors];
 
-  // Read light sensors.
-  for (int i = 0; i < numberLightSensors; i++) {
-    lightSensorsReading[i] = readLightSensorDigital(i);
+    // Read light sensors.
+    for (int i = 0; i < numberLightSensors; i++) {
+      lightSensorsReading[i] = readLightSensorDigital(i);
+    }
+
+    // Get error based on the light sensors reading.
+    int error = getError(lightSensorsReading);
+    // Serial.println(error);
+
+    int speedA = 0;
+    int speedB = 0;
+
+    // Calculate PID value based on error and Kp and Kd constants.
+    calculatePID(error, Kp, Kd, &speedA, &speedB);
+
+    // Move motors with speeds returned by PID algorhitm.
+    moveTank(speedA, speedB);
+
+    // If data from bluetooth are available.
+    if (BT.available()) {
+      // Get Kp and Kd from the app.
+      getBluetoothData(&Kp, &Kd);
+
+      // Debug messages.
+      Serial.print(Kp);
+      Serial.print("\t");
+      Serial.print(Kd);
+      Serial.print("\n");
+    }
   }
 
-  // Get error based on the light sensors reading.
-  int error = getError(lightSensorsReading);
-  // Serial.println(error);
-
-  int speedA = 0;
-  int speedB = 0;
-
-  // Calculate PID value based on error and Kp and Kd constants.
-  calculatePID(error, Kp, Kd, &speedA, &speedB);
-  // Serial.println(speedA);
-  // Serial.println(speedB);
-  // Move motors with speeds returned by PID algorhitm.
-  moveTank(speedA, speedB);
-
-  if (BT.available()) {
-    getBluetoothData(&Kp, &Kd);
-    Serial.print(Kp);
-    Serial.print("\t");
-    Serial.print(Kd);
-    Serial.print("\n");
+  else {
+    forward(0);
   }
+
+  if (digitalRead(buttonPin) == LOW && previous == HIGH && millis() - time > debounce) {
+    if (running == HIGH)
+      running = LOW;
+    else
+      running = HIGH;
+
+    time = millis();
+  }
+
+  previous = digitalRead(buttonPin);
 }
 
 void calculatePID(int error, int Kp, int Kd, int *speedA, int *speedB) {
@@ -130,22 +163,19 @@ int getError(int *sensorsReadValue) {
 }
 
 void getBluetoothData(float *Kp, float *Kd) {
-  String data = "";
-  while (BT.available()) {
-    char ReadChar = (char)BT.read();
+  int data[3];
 
-    if (ReadChar == ')') {
-      break;
-    } else {
-      data += ReadChar;
-    }
+  int i = 0;
+  while (BT.available()) {
+    data[i] = BT.read();
+    i++;
   }
-  int index0 = data.indexOf(':');
-  int index1 = data.indexOf(':', index0 + 1);
-  int index2 = data.indexOf(':', index1 + 1);
-  int kp = data.substring(0, index0).toInt();
-  int ki = data.substring(index0 + 1, index1).toInt();
-  int kd = data.substring(index1 + 1, index2).toInt();
-  *Kp = kp;
-  *Kd = kd;
+
+  *Kp = data[0];
+  *Kd = data[2];
+}
+
+void save() {
+  EEPROM.write(0, Kp);
+  EEPROM.write(1, Kd);
 }
